85258889b1816c77d65caa79a8e1fb1d98f85f45
N. Escobar <nick@nescobar.media>
2025-07-10 14:06:08 -0400

    moved doc generation to class model

--- scripts/generate_docs.js
+++ scripts/generate_docs.js
@@ -1,11 +1,9 @@
-//const { program } = require('commander');
-//const pkgjson = require('../package.json');
-
 import { program } from 'commander';
 import pkgjson from '../package.json' with { type: "json" };
 
-function generate_install(){
-    console.log(
+class DocStrings {
+
+    static install=
 `Install \`${pkgjson.name}\` via \`npm\`.
 
 To install \`${pkgjson.name}\` as a library in a JavaScript or TypeScript project:
@@ -17,32 +15,30 @@ To install \`${pkgjson.name}\` globally, for use as a CLI tool:
 \`\`\`sh
 npm install -g ${pkgjson.name}
 \`\`\``
-    );
+
+    static about=
+`${pkgjson.description}`
+
+    static license=
+`\`${pkgjson.name}\` is released under the **${pkgjson.license}** license. For more information, see the repository's [LICENSE](/LICENSE) file.`
 }
-function generate_about(){
-    console.log(pkgjson.description);
-}
-function generate_license(){
-    console.log(
-`\`${pkgjson.name}\` is released under the **${pkgjson.license}** license. For more information, see the repository's [LICENSE](/LICENSE) file.`);
-}
+
+program
+ .command('about')
+ .action(() => {
+    console.log(DocStrings.about);
+ });
 
 program
  .command('install')
  .action(() => {
-    generate_install();
+    console.log(DocStrings.install);
  });
 
 program
  .command('license')
  .action(() => {
-    generate_license();
- });
-
-program
- .command('about')
- .action(() => {
-    generate_about();
+    console.log(DocStrings.license);
  });
 
 program.parse();
